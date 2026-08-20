634745ac13b87340839f1facbb4298edb69275eb
admin0115com-png <admin0115.com@gmail.com>
2026-08-20 22:40:10 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -950,8 +950,7 @@ export default function App() {
                 </div>
               </div>
             </div>
-        
-       
+        )}
         {route.view === 'legal-terms' && <LegalDocuments view="terms" setView={handleSetView} />}
         
         {route.view === 'legal-privacy' && <LegalDocuments view="privacy" setView={handleSetView} />}
